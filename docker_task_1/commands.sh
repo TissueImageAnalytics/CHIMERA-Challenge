b8d3da58d67fb5445dbd6fe2a29c5730c1d728ee
Jiaqi Lv <lvjiaqi9@gmail.com>
2025-08-21 17:27:30 +0100

    update

--- docker_task_1/commands.sh
+++ docker_task_1/commands.sh
@@ -42,23 +42,23 @@ fi
 # python convert_tissue_mask_for_trident.py
 # echo "Finished convert_tissue_mask_for_trident.py"
 
-echo "Running TITAN"
-cd /opt/app/TRIDENT
+# echo "Running TITAN"
+# cd /opt/app/TRIDENT
 
-python run_batch_of_slides.py \
-  --task all \
-  --max_workers 8 \
-  --wsi_dir /input/images/prostatectomy-wsi \
-  --job_dir /output/trident_processed \
-  --segmenter grandqc \
-  --slide_encoder titan \
-  --patch_encoder conch_v15 \
-  --patch_encoder_ckpt /home/user/.cache/huggingface/modules/transformers_modules/titan/conch_v1_5_pytorch_model.bin \
-  --mag 10 \
-  --patch_size 1024 \
-  --batch_size 32
+# python run_batch_of_slides.py \
+#   --task all \
+#   --max_workers 8 \
+#   --wsi_dir /input/images/prostatectomy-wsi \
+#   --job_dir /output/trident_processed \
+#   --segmenter grandqc \
+#   --slide_encoder titan \
+#   --patch_encoder conch_v15 \
+#   --patch_encoder_ckpt /home/user/.cache/huggingface/modules/transformers_modules/titan/conch_v1_5_pytorch_model.bin \
+#   --mag 10 \
+#   --patch_size 1024 \
+#   --batch_size 32
 
-echo "TITAN finished OK"
+# echo "TITAN finished OK"
 
 # echo "Running PRISM"
 # cd /opt/app/TRIDENT
@@ -78,6 +78,6 @@ echo "TITAN finished OK"
 # echo running task_1_inference_template.py
 cd /opt/app
 
-echo running task_1_inference_710.py
-exec python task_1_inference_710.py
+echo running clinical_only_inference.py
+exec python clinical_only_inference.py
 echo finished
